702eccf1ac5ea10996b5bbdac5e6b04c7b04323d
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-07-11 15:59:34 +0900

    End of day 7/11

--- script.js
+++ script.js
@@ -51,17 +51,7 @@ renderPrizes();
 renderCases();
 
 //////////////////////////////////////////////////////////////////////////////
-// Bank offer
-const offerFunc = function () {
-  const offer = prizes.reduce((acc, prize, _, arr) => {
-    return acc + prize * (1 / arr.length);
-  }, 0);
-  console.log(`Off: ${Math.round((offer / 100) * 100)}`);
-  return Math.round((offer / 100) * 100);
-};
-
-//////////////////////////////////////////////////////////////////////////////
-// Case elim
+// Game logic
 const imgDiv = document.querySelectorAll(".img-div");
 const playerNum = document.querySelector(".player-number");
 const playerCase = document.querySelector(".player-case");
@@ -69,15 +59,20 @@ const prizesAll = document.querySelectorAll(".prize");
 const promptMsg = document.querySelector(".prompt-msg");
 const caseRevealModal = document.querySelector(".case-reveal-modal");
 const caseAmount = document.querySelector(".case-amount");
+const offerModal = document.querySelector(".offer-modal");
+const offerAmount = document.querySelector(".offer-amount");
+const noDealBtn = document.querySelector(".no-deal-btn");
+const prevOffersList = document.querySelector(".previous-offers-list");
 
 const oneMoreCaseMsg = "Please choose 1 more case to open.";
 let chosenCase = null;
+const hideFunc = (el) => el.classList.toggle("hidden");
 
 const caseElim = function () {
   imgDiv.forEach((div) => {
     div.addEventListener("click", function () {
       ///////////////////////////////////CALCULATE CASE AMOUNT/////////////////////////////////
-      if (chosenCase > 0) {
+      if (chosenCase) {
         const i = Math.round(Math.random() * (prizes.length - 1));
         console.log(i);
         const prizeAmount = `$${prizes[i].toLocaleString("en-US")}`;
@@ -85,7 +80,8 @@ const caseElim = function () {
         console.log(prizeAmount);
 
         ///////////////////////////////////HIDE CASES & PRIZES/////////////////////////////////
-        caseRevealModal.classList.remove("hidden");
+
+        hideFunc(caseRevealModal);
 
         setTimeout(function () {
           caseRevealModal.style.backgroundImage =
@@ -93,7 +89,7 @@ const caseElim = function () {
           caseAmount.innerText = prizeAmount;
 
           setTimeout(() => {
-            caseRevealModal.classList.add("hidden");
+            hideFunc(caseRevealModal);
             prizesAll.forEach((prize) => {
               if (prize.textContent === prizeAmount) {
                 prize.style.opacity = 0;
@@ -121,67 +117,72 @@ const caseElim = function () {
               promptMsgFunc(15);
               promptMsgFunc(20);
             });
+
+            ///////////////////////////////////BANK OFFER/////////////////////////////////
+            const offerFunc = function (percent) {
+              hideFunc(offerModal);
+              const offer = prizes.reduce((acc, prize, _, arr) => {
+                return acc + prize * (1 / arr.length);
+              }, 0);
+              return Math.round(
+                (((offer / 100) * 100 * percent) / 100) * 100
+              ).toLocaleString("en-US");
+            };
+
+            const prevOffersFunc = (i) => {
+              const li = document.createElement("li");
+              li.innerText = offers[i];
+              prevOffersList.appendChild(li);
+            };
+
+            const offers = [];
+
+            const offerRoundFunc = function () {
+              if (prizes.length === 20) {
+                const offer1 = (offerAmount.innerText = `$${offerFunc(0.2)}`);
+                offers.push(offer1);
+                console.log(offers[0]);
+              }
+              if (prizes.length === 15) {
+                const offer2 = (offerAmount.innerText = `$${offerFunc(0.3)}`);
+                offers.push(offer2);
+                console.log(offers);
+                prevOffersFunc(0);
+              }
+              if (prizes.length === 11) {
+                const offer3 = (offerAmount.innerText = `$${offerFunc(0.6)}`);
+              }
+              if (prizes.length === 8) {
+                const offer4 = (offerAmount.innerText = `$${offerFunc(0.8)}`);
+              }
+              if (prizes.length === 6) {
+                const offer5 = (offerAmount.innerText = `$${offerFunc(0.9)}`);
+              }
+              if (prizes.length === 5) {
+                const offer6 = (offerAmount.innerText = `$${offerFunc(1)}`);
+              }
+              if (prizes.length === 4) {
+                const offer7 = (offerAmount.innerText = `$${offerFunc(1.1)}`);
+              }
+              if (prizes.length === 3) {
+                const offer8 = (offerAmount.innerText = `$${offerFunc(1.2)}`);
+              }
+              if (prizes.length === 2) {
+                const offer9 = (offerAmount.innerText = `$${offerFunc(1.25)}`);
+              }
+            };
+            offerRoundFunc();
+
+            noDealBtn.addEventListener("click", () => {
+              offerModal.classList.add("hidden");
+            });
           }, 1800);
         }, 1500);
-        ///////////////////////////////////BANK OFFER/////////////////////////////////
-        if (prizes.length === 20)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 0.2) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 15)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 0.3) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 11)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 0.6) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 8)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 0.8) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 6)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 0.9) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 5)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 1.1) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 4)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 1.15) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 3)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 1.25) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
-        if (prizes.length === 2)
-          console.log(
-            `Offer: $${(
-              Math.round((offerFunc() * 1.25) / 100) * 100
-            ).toLocaleString("en-US")}`
-          );
       }
       if (chosenCase === null) {
         chosenCase = playerNum.innerText = div.lastElementChild.innerText;
-        div.classList.add("hidden");
+        hideFunc(div);
+        promptMsg.innerText = `Please choose 6 more cases to open`;
       }
     });
   });
